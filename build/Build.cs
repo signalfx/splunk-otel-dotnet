211a90a496c14a6b3766b56143973f4234df3e12
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-08-30 11:51:11 +0200

    base builds

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
-using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
 
 class Build : NukeBuild
 {
@@ -21,24 +11,42 @@ class Build : NukeBuild
 
     public static int Main () => Execute<Build>(x => x.Compile);
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("Configuration to build - Default is 'Release'")]
+    readonly Configuration Configuration = Configuration.Release;
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetClean();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetTasks.DotNetRestore();
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetTasks.DotNetBuild(s => s
+                    .SetNoRestore(true)
+                    .SetConfiguration(Configuration));
         });
 
+    Target Test => _ => _
+        .After(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(s => s
+                .SetNoBuild(true)
+                .SetConfiguration(Configuration));
+        });
+
+    Target Workflow => _ => _
+        .DependsOn(Clean)
+        .DependsOn(Compile)
+        .DependsOn(Test);
 }
