6244d54fa21741063a4889889be13a637c72580f
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2022-08-30 14:17:18 +0200

    remove redundant comments

--- build/Build.cs
+++ build/Build.cs
@@ -3,12 +3,6 @@ using Nuke.Common.Tools.DotNet;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Release'")]
