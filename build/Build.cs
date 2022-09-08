46afb31a4091d7007f37352eb65d27e70339c195
Dawid Szmigielski <mr.dawid.szmigielski@gmail.com>
2022-09-08 11:24:15 +0200

    Extend http timeout (#11)

--- build/Build.cs
+++ build/Build.cs
@@ -39,7 +39,12 @@ class Build : NukeBuild
 
             var uri =
                 $"https://github.com/open-telemetry/opentelemetry-dotnet-instrumentation/releases/download/{OpenTelemetryAutoInstrumentationVersion}/{fileName}";
-            await HttpTasks.HttpDownloadFileAsync(uri, BinDirectory / fileName);
+
+            await HttpTasks.HttpDownloadFileAsync(uri, BinDirectory / fileName, clientConfigurator: httpClient =>
+            {
+                httpClient.Timeout = TimeSpan.FromMinutes(3);
+                return httpClient;
+            });
         });
 
     static string GetOTelAutoInstrumentationFileName()
