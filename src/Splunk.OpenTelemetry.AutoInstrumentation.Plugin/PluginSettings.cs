7376bc41c9d694c9d1a4262614ca5ddbab813a73
Rasmus Kuusmann <rkuusmann@splunk.com>
2022-11-28 13:25:31 +0200

    Support SPLUNK_REALM and SPLUNK_ACCESS_TOKEN (#78)
    * Support Splunk realm and access token
    
    Signed-off-by: RassK <rkuusmann@splunk.com>
    
    * fix formatting and licensing
    
    * Fix endpoints priority
    
    * fixes and minor restructuring
    
    * fix settings tests
    
    Signed-off-by: RassK <rkuusmann@splunk.com>
    Co-authored-by: Piotr Kiełkowicz <pkiekowicz@splunk.com>

---
+++ src/Splunk.OpenTelemetry.AutoInstrumentation.Plugin/PluginSettings.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="PluginSettings.cs" company="Splunk Inc.">
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using Splunk.OpenTelemetry.AutoInstrumentation.Plugin.Configuration;
+
+namespace Splunk.OpenTelemetry.AutoInstrumentation.Plugin;
+
+internal class PluginSettings
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginSettings"/> class
+    /// using the specified <see cref="IConfigurationSource"/> to initialize values.
+    /// </summary>
+    /// <param name="source">The <see cref="IConfigurationSource"/> to use when retrieving configuration values.</param>
+    internal PluginSettings(IConfigurationSource source)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        Realm = source.GetString(ConfigurationKeys.Splunk.Realm);
+        AccessToken = source.GetString(ConfigurationKeys.Splunk.AccessToken);
+        IsOtlpEndpointSet = !string.IsNullOrEmpty(source.GetString(ConfigurationKeys.OpenTelemetry.OtlpEndpoint));
+    }
+
+    public bool IsOtlpEndpointSet { get; set; }
+
+    public string? Realm { get; set; }
+
+    public string? AccessToken { get; set; }
+
+    public static PluginSettings FromDefaultSources()
+    {
+        var configurationSource = new CompositeConfigurationSource
+        {
+            new EnvironmentConfigurationSource(),
+
+/*
+* TODO: Enable ASP.NET web.config support
+*
+#if NETFRAMEWORK
+            // on .NET Framework only, also read from app.config/web.config
+            new NameValueConfigurationSource(System.Configuration.ConfigurationManager.AppSettings)
+#endif
+*/
+        };
+
+        return new PluginSettings(configurationSource);
+    }
+}
---
+++ test/Splunk.OpenTelemetry.AutoInstrumentation.Plugin.Tests/ExporterTests.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="ExporterTests.cs" company="Splunk Inc.">
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using OpenTelemetry.Exporter;
+
+namespace Splunk.OpenTelemetry.AutoInstrumentation.Plugin.Tests
+{
+    public class ExporterTests
+    {
+        [Fact]
+        public void ConfigureOtlpOptions_EndpointSpecified()
+        {
+            const string endpoint = "https://specifically-specified-endpoint.com";
+
+            Environment.SetEnvironmentVariable("SPLUNK_REALM", "my-realm");
+            Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", endpoint);
+
+            var settings = PluginSettings.FromDefaultSources();
+
+            var options = new OtlpExporterOptions();
+            new Metrics(settings).ConfigureOptions(options);
+
+            options.Endpoint.Should().Be(endpoint);
+
+            Environment.SetEnvironmentVariable("SPLUNK_REALM", null);
+            Environment.SetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT", null);
+        }
+    }
+}
