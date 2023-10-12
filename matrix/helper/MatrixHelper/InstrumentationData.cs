4719fc91549726d11c64242ea9eaf57fe1f99b30
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-10-12 10:05:56 +0200

    Multiple libraries in instrumentation

--- matrix/helper/MatrixHelper/InstrumentationData.cs
+++ matrix/helper/MatrixHelper/InstrumentationData.cs
@@ -29,28 +29,28 @@ public static class InstrumentationData
 
         var instrumentations = new Instrumentation[]
         {
-            new(new[] {"ASPNET"}, "ASP.NET Framework (.NET Framework)", "See :ref:`dotnet-otel-versions`", "MVC / WebApi (Only integrated pipeline mode supported). Metrics requires trace instrumentation.", "beta", "community", new [] {new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.AspNet", "1.0.0-rc9.9", "beta"), new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule", "1.0.0-rc9.9", "beta") }, new SignalsList[]{new TracesList(), new MetricList(new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) }) {ToDoComment = "supportedVersions with ref link"},
-            new("ASPNETCORE", "ASP.NET Core", "See :ref:`dotnet-otel-versions`", "Metrics automatically activates `Microsoft.AspNetCore.Hosting.HttpRequestIn` spans.", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.AspNetCore", "1.5.1-beta.1", "beta"), new SignalsList[]{new TracesList(), new MetricList(new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) }) {ToDoComment = "reflink in supportedVersions"},
-            new("AZURE", "Azure SDK", "`Azure.` prefixed packages, released after October 1, 2021", null, "beta", "third-party", new TracesList()),
-            new("ELASTICSEARCH", "Elastic.Clients.Elasticsearch", "8.0.0 and higher", null, "beta", "third-party", new TracesList()),
-            new("ENTITYFRAMEWORKCORE", "Microsoft.EntityFrameworkCore", "6.0.12 and higher", "Not supported on .NET Framework", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.EntityFrameworkCore", "1.0.0-beta.7", "beta"), new SignalsList[]{new TracesList()}),
-            new("GRAPHQL", "GraphQL", "7.5.0 and higher", "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
-            new("GRPCNETCLIENT", "Grpc.Net.Client", "2.52.0 to 3.0.0", null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.GrpcNetClient", "1.5.1-beta.1", "beta"), new SignalsList[] { new TracesList() }),
-            new("HTTPCLIENT", "System.Net.Http.HttpClient and System.Net.HttpWebRequest", "See :ref:`dotnet-otel-versions`", null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.Http", "1.5.1-beta.1", "beta"), new SignalsList[] { new TracesList(), new MetricList(new MetricData("http.client.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of outbound HTTP requests, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs."), new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) } ) {ToDoComment = "how to handle two libraries? convert to array?, reflink in versions"},
-            new("MASSTRANSIT", "MassTransit", "8.0.0 and higher", "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
-            new("MONGODB", "MongoDB.Driver.Core", "2.13.3 to 3.0.0", "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
-            new("MYSQLCONNECTOR", "MySqlConnector", "2.0.0 and higher", null, "beta", "third-party", new TracesList()),
-            new("MYSQLDATA", "MySql.Data", "8.1.0 and higher", "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
-            new("NPGSQL", "Npgsql", "6.0.0 and higher", null, "beta", "third-party", new TracesList()),
-            new(new[] {"NSERVICEBUS"}, "NServiceBus", "8.0.0 and higher", null, "beta", "third-party", Array.Empty<Dependency>(), new SignalsList[]{new TracesList(), new MetricList(new MetricData("nservicebus.messaging.successes", "Cumulative counter", "Number of messages successfully processed by the endpoint."), new MetricData("nservicebus.messaging.fetches", "Cumulative counter", "Number of messages retrieved from the queue by the endpoint."), new MetricData("nservicebus.messaging.failures", "Cumulative counter", "Number of messages unsuccessfully processed by the endpoint."))}),
-            new("QUARTZ", "Quartz", "3.4.0 and higher", "Not supported on .NET Framework 4.7.1 and lower", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Quartz", "1.0.0-alpha.3", "alpha"), new SignalsList[] { new TracesList() } ),
-            new("STACKEXCHANGEREDIS", "StackExchange.Redis", "2.0.405 to 3.0.0", "Not supported on .NET Framework", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.StackExchangeRedis", "1.0.0-rc9.10", "beta"), new SignalsList[] { new TracesList() } ),
-            new(new []{"WCFCLIENT", "WCFSERVICE" }, "System.ServiceModel", "4.7.0 and higher of `System.ServiceModel.Primitives`", "Service side not supported on .NET. `WCFCLIENT for client side instrumentation and `WCFSERVICE` for service side instrumentation", "beta", "community", new []{ new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Wcf", "1.0.0-rc.12", "beta") }, new SignalsList[] { new TracesList() }),
+            new(new[] {"ASPNET"}, new [] {new InstrumentedLibrary("ASP.NET Framework (.NET Framework)", "See :ref:`dotnet-otel-versions`")}, "MVC / WebApi (Only integrated pipeline mode supported). Metrics requires trace instrumentation.", "beta", "community", new [] {new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.AspNet", "1.0.0-rc9.9", "beta"), new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule", "1.0.0-rc9.9", "beta") }, new SignalsList[]{new TracesList(), new MetricList(new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) }) {ToDoComment = "supportedVersions with ref link"},
+            new("ASPNETCORE", new InstrumentedLibrary("ASP.NET Core", "See :ref:`dotnet-otel-versions`"), "Metrics automatically activates `Microsoft.AspNetCore.Hosting.HttpRequestIn` spans.", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.AspNetCore", "1.5.1-beta.1", "beta"), new SignalsList[]{new TracesList(), new MetricList(new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) }) {ToDoComment = "reflink in supportedVersions"},
+            new("AZURE", new InstrumentedLibrary("Azure SDK", "`Azure.` prefixed packages, released after October 1, 2021"), null, "beta", "third-party", new TracesList()),
+            new("ELASTICSEARCH", new InstrumentedLibrary("Elastic.Clients.Elasticsearch", "8.0.0 and higher"), null, "beta", "third-party", new TracesList()),
+            new("ENTITYFRAMEWORKCORE", new InstrumentedLibrary("Microsoft.EntityFrameworkCore", "6.0.12 and higher"), "Not supported on .NET Framework", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.EntityFrameworkCore", "1.0.0-beta.7", "beta"), new SignalsList[]{new TracesList()}),
+            new("GRAPHQL", new InstrumentedLibrary("GraphQL", "7.5.0 and higher"), "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
+            new("GRPCNETCLIENT", new InstrumentedLibrary("Grpc.Net.Client", "2.52.0 to 3.0.0"), null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.GrpcNetClient", "1.5.1-beta.1", "beta"), new SignalsList[] { new TracesList() }),
+            new(new [] {"HTTPCLIENT"}, new[] {new InstrumentedLibrary("System.Net.Http.HttpClient", "See :ref:`dotnet-otel-versions`"),  new InstrumentedLibrary("System.Net.HttpWebRequest", "See :ref:`dotnet-otel-versions`") }, null, "beta", "community", new[] { new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/src/OpenTelemetry.Instrumentation.Http", "1.5.1-beta.1", "beta") }, new SignalsList[] { new TracesList(), new MetricList(new MetricData("http.client.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of outbound HTTP requests, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs."), new MetricData("http.server.duration_{bucket|count|sum}", "Cumulative counters (histogram)", "Duration of the inbound HTTP request, in the form of count, sum, and histogram buckets. This metric originates multiple metric time series, which might result in increased data ingestion costs.")) } ) {ToDoComment = "how to handle two libraries? convert to array?, reflink in versions"},
+            new("MASSTRANSIT", new InstrumentedLibrary("MassTransit", "8.0.0 and higher"), "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
+            new("MONGODB", new InstrumentedLibrary("MongoDB.Driver.Core", "2.13.3 to 3.0.0"), "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
+            new("MYSQLCONNECTOR", new InstrumentedLibrary("MySqlConnector", "2.0.0 and higher"), null, "beta", "third-party", new TracesList()),
+            new("MYSQLDATA", new InstrumentedLibrary("MySql.Data", "8.1.0 and higher"), "Not supported on .NET Framework", "beta", "third-party", new TracesList()),
+            new("NPGSQL", new InstrumentedLibrary("Npgsql", "6.0.0 and higher"), null, "beta", "third-party", new TracesList()),
+            new(new[] {"NSERVICEBUS"}, new [] {new InstrumentedLibrary("NServiceBus", "8.0.0 and higher")}, null, "beta", "third-party", Array.Empty<Dependency>(), new SignalsList[]{new TracesList(), new MetricList(new MetricData("nservicebus.messaging.successes", "Cumulative counter", "Number of messages successfully processed by the endpoint."), new MetricData("nservicebus.messaging.fetches", "Cumulative counter", "Number of messages retrieved from the queue by the endpoint."), new MetricData("nservicebus.messaging.failures", "Cumulative counter", "Number of messages unsuccessfully processed by the endpoint."))}),
+            new("QUARTZ", new InstrumentedLibrary("Quartz", "3.4.0 and higher"), "Not supported on .NET Framework 4.7.1 and lower", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Quartz", "1.0.0-alpha.3", "alpha"), new SignalsList[] { new TracesList() } ),
+            new("STACKEXCHANGEREDIS", new InstrumentedLibrary("StackExchange.Redis", "2.0.405 to 3.0.0"), "Not supported on .NET Framework", "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.StackExchangeRedis", "1.0.0-rc9.10", "beta"), new SignalsList[] { new TracesList() } ),
+            new(new []{"WCFCLIENT", "WCFSERVICE" }, new []{new InstrumentedLibrary("System.ServiceModel", "4.7.0 and higher of `System.ServiceModel.Primitives`")}, "Service side not supported on .NET. `WCFCLIENT for client side instrumentation and `WCFSERVICE` for service side instrumentation", "beta", "community", new []{ new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Wcf", "1.0.0-rc.12", "beta") }, new SignalsList[] { new TracesList() }),
 
-            new("NETRUNTIME", ".NET runtime", "any", null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Runtime", "1.5.1", "stable"), new SignalsList[]{new MetricList(netRuntimeMetrics) }),
-            new("PROCESS", "Process", "any", null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Process", "0.5.0-beta.3", "beta"), new SignalsList[]{new MetricList(new MetricData("process.memory.usage", "Gauge", "The amount of physical memory allocated for this process."), new ("process.memory.virtual", "Gauge", "The amount of committed virtual memory for this process."), new MetricData("process.cpu.time", "Cumulative counter", "Total CPU seconds broken down by different states, such as user and system."), new("process.cpu.count", "Gauge", "Total CPU seconds broken down by different states, such as user and system."), new("process.threads", "Gauge", "Process threads count."))}),
+            new("NETRUNTIME", new InstrumentedLibrary(".NET runtime", null), null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Runtime", "1.5.1", "stable"), new SignalsList[]{new MetricList(netRuntimeMetrics) }),
+            new("PROCESS", new InstrumentedLibrary("Process", null), null, "beta", "community", new Dependency("https://github.com/open-telemetry/opentelemetry-dotnet-contrib/tree/main/src/OpenTelemetry.Instrumentation.Process", "0.5.0-beta.3", "beta"), new SignalsList[]{new MetricList(new MetricData("process.memory.usage", "Gauge", "The amount of physical memory allocated for this process."), new ("process.memory.virtual", "Gauge", "The amount of committed virtual memory for this process."), new MetricData("process.cpu.time", "Cumulative counter", "Total CPU seconds broken down by different states, such as user and system."), new("process.cpu.count", "Gauge", "Total CPU seconds broken down by different states, such as user and system."), new("process.threads", "Gauge", "Process threads count."))}),
 
-            new("ILOGGER", "Microsoft.Extensions.Logging", "6.0.0 and higher", "Not supported on .NET Framework", "beta", "community", new LogLists())
+            new("ILOGGER", new InstrumentedLibrary("Microsoft.Extensions.Logging", "6.0.0 and higher"), "Not supported on .NET Framework", "beta", "community", new LogLists())
         };
 
         return instrumentations;
@@ -59,21 +59,20 @@ public static class InstrumentationData
 
 public class Instrumentation
 {
-    public Instrumentation(string key, string library, string supportedVersions, string? comment, string stability, string supportabilityLevel, SignalsList signalsList)
-        : this(new[] {key}, library, supportedVersions, comment, stability, supportabilityLevel, Array.Empty<Dependency>(), new[] {signalsList})
+    public Instrumentation(string key, InstrumentedLibrary library, string? comment, string stability, string supportabilityLevel, SignalsList signalsList)
+        : this(new[] {key}, new [] { library }, comment, stability, supportabilityLevel, Array.Empty<Dependency>(), new[] {signalsList})
     {
     }
 
-    public Instrumentation(string key, string library, string supportedVersions, string? comment, string stability, string supportabilityLevel, Dependency dependency, SignalsList[] signalsList)
-        : this(new[] { key }, library, supportedVersions, comment, stability, supportabilityLevel, new[] { dependency }, signalsList)
+    public Instrumentation(string key, InstrumentedLibrary library, string? comment, string stability, string supportabilityLevel, Dependency dependency, SignalsList[] signalsList)
+        : this(new[] { key }, new[] { library }, comment, stability, supportabilityLevel, new[] { dependency }, signalsList)
     {
     }
 
-    public Instrumentation(string[] keys, string library, string supportedVersions, string? comment, string stability, string supportabilityLevel, Dependency[] dependencies, SignalsList[] signalsList)
+    public Instrumentation(string[] keys, InstrumentedLibrary[] instrumentedLibraries, string? comment, string stability, string supportabilityLevel, Dependency[] dependencies, SignalsList[] signalsList)
     {
         Keys = keys;
-        Library = library;
-        SupportedVersions = supportedVersions;
+        InstrumentedLibraries = instrumentedLibraries;
         Comment = comment;
         Stability = stability;
         SupportabilityLevel = supportabilityLevel;
@@ -83,9 +82,7 @@ public class Instrumentation
 
     public string[] Keys { get; set; }
 
-    public string Library { get; set; }
-
-    public string SupportedVersions { get; set; }
+    public InstrumentedLibrary[] InstrumentedLibraries { get; set; }
 
     [YamlMember(DefaultValuesHandling = DefaultValuesHandling.OmitNull)]
     public string? Comment { get; set; }
@@ -152,4 +149,18 @@ public class Trace
 public class TracesList: SignalsList
 {
     public Trace[] Traces { get; set; }
+}
+
+public class InstrumentedLibrary
+{
+    public InstrumentedLibrary(string name, string? supportedVersions)
+    {
+        Name = name;
+        SupportedVersions = supportedVersions;
+    }
+
+    public string Name { get; set; }
+
+    [YamlMember(DefaultValuesHandling = DefaultValuesHandling.OmitNull)]
+    public string? SupportedVersions { get; set; }
 }
