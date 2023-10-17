12ce8cc8e2454821554cafc936865f8388e5fe5d
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-10-17 12:44:55 +0200

    Link -> source href

--- matrix/helper/MatrixHelper/Dependency.cs
+++ matrix/helper/MatrixHelper/Dependency.cs
@@ -2,16 +2,16 @@
 
 public class Dependency
 {
-    public Dependency(string name, string link, string version, string stability)
+    public Dependency(string name, string sourceHref, string version, string stability)
     {
         Name = name;
-        Link = link;
+        SourceHref = sourceHref;
         Version = version;
         Stability = stability;
     }
 
     public string Name { get; set; }
-    public string Link { get; set; }
+    public string SourceHref { get; set; }
     public string Version { get; set; }
     public string Stability { get; set; }  
 }
