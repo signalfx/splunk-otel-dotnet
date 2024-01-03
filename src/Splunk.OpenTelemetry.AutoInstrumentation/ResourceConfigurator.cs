d8fc6e4e2427ccb71e2a33830d0d983f7c8cbb98
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-01-03 05:59:47 +0100

    {PR feedback - remove fallback for service.name it is now correctly handled by upstream

--- src/Splunk.OpenTelemetry.AutoInstrumentation/ResourceConfigurator.cs
+++ src/Splunk.OpenTelemetry.AutoInstrumentation/ResourceConfigurator.cs
@@ -14,9 +14,7 @@
 // limitations under the License.
 // </copyright>
 
-using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using OpenTelemetry.Resources;
 #if NET6_0_OR_GREATER
 using Splunk.OpenTelemetry.AutoInstrumentation.ContinuousProfiler;
@@ -29,7 +27,6 @@ internal static class ResourceConfigurator
     /// <summary>
     /// splunk.distro.version is deprecated
     /// </summary>
-    private const string ServiceNameAttribute = "service.name";
     private const string SplunkDistroVersionName = "splunk.distro.version";
     private const string TelemetryDistroNameName = "telemetry.distro.name";
     private const string TelemetryDistroNameValue = "splunk-otel-dotnet";
@@ -66,47 +63,7 @@ internal static class ResourceConfigurator
 
 #if NET6_0_OR_GREATER
         var resource = resourceBuilder.Build();
-
-        if (resource.Attributes.All(kvp => kvp.Key != ServiceNameAttribute))
-        {
-            var localResourceBuilder = ResourceBuilder.CreateEmpty();
-            localResourceBuilder.AddAttributes(resource.Attributes);
-            // service.name was not configured yet use the fallback.
-            localResourceBuilder.AddAttributes(new KeyValuePair<string, object>[] { new(ServiceNameAttribute, ServiceNameConfigurator.GetFallbackServiceName()) });
-            SampleExporter.SetResources(localResourceBuilder.Build());
-        }
-        else
-        {
-            SampleExporter.SetResources(resource);
-        }
+        SampleExporter.SetResources(resource);
 #endif
     }
-
-#if NET6_0_OR_GREATER
-    private static class ServiceNameConfigurator
-    {
-        internal static string GetFallbackServiceName()
-        {
-            return Assembly.GetEntryAssembly()?.GetName().Name ?? GetCurrentProcessName();
-        }
-
-        /// <summary>
-        /// <para>Wrapper around <see cref="Process.GetCurrentProcess"/> and <see cref="Process.ProcessName"/></para>
-        /// <para>
-        /// On .NET Framework the <see cref="Process"/> class is guarded by a
-        /// LinkDemand for FullTrust, so partial trust callers will throw an exception.
-        /// This exception is thrown when the caller method is being JIT compiled, NOT
-        /// when Process.GetCurrentProcess is called, so this wrapper method allows
-        /// us to catch the exception.
-        /// </para>
-        /// </summary>
-        /// <returns>Returns the name of the current process.</returns>
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static string GetCurrentProcessName()
-        {
-            using var currentProcess = Process.GetCurrentProcess();
-            return currentProcess.ProcessName;
-        }
-    }
-#endif
 }
