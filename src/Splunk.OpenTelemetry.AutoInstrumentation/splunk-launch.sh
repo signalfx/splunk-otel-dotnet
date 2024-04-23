27b83db61b9d0a591fb7b24aaa032cd45b04efc3
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-04-23 09:12:21 +0200

    SC2046

--- src/Splunk.OpenTelemetry.AutoInstrumentation/splunk-launch.sh
+++ src/Splunk.OpenTelemetry.AutoInstrumentation/splunk-launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # This script is expected to be used in a build that specified a RuntimeIdentifier (RID)
-BASE_PATH="$(cd $(dirname "$0") && pwd)"
+BASE_PATH="$(cd "$(dirname "$0")" && pwd)"
 
 export OTEL_DOTNET_AUTO_PLUGINS="Splunk.OpenTelemetry.AutoInstrumentation.Plugin, Splunk.OpenTelemetry.AutoInstrumentation"
 
