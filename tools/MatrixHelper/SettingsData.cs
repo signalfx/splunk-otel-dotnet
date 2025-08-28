d55b3578f0931d9fb2bd5104f3bd05e6a6a3f9e7
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-08-28 09:22:11 +0200

    Include env. variable into matrix

--- tools/MatrixHelper/SettingsData.cs
+++ tools/MatrixHelper/SettingsData.cs
@@ -60,6 +60,7 @@ internal static class SettingsData
             new("SPLUNK_PROFILER_MEMORY_ENABLED", "Activates memory profiling.", "false", "boolean", ProfilingCategory),
             new("SPLUNK_PROFILER_LOGS_ENDPOINT", "The collector endpoint for profiler logs.", "http://localhost:4318/v1/logs", "string", ProfilingCategory),
             new("SPLUNK_PROFILER_CALL_STACK_INTERVAL", "Frequency with which call stacks are sampled, in milliseconds.", "10000", "int", ProfilingCategory),
+            new("SPLUNK_PROFILER_EXPORT_TIMEOUT", "Export timeout, in milliseconds.", "3000", "int", ProfilingCategory),
 
             // trace propagation
             new("OTEL_PROPAGATORS", "Comma-separated list of propagators for the tracer. The default value is `tracecontext,baggage`. Supported values are `b3multi`, `b3`, `tracecontext`, and `baggage`.", "tracecontext,baggage", "string", TracePropagationCategory),
