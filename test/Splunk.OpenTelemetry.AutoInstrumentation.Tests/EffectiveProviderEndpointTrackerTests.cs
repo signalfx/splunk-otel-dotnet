de99a208bcba3fe4fad02fb9a6a92c08e628425d
Mateusz Łach <mateusza@splunk.com>
2026-07-09 07:23:40 +0200

    [effective-config] adjust effective config reporting to gdi-spec (#830)

---
+++ test/Splunk.OpenTelemetry.AutoInstrumentation.Tests/EffectiveProviderEndpointTrackerTests.cs
@@ -0,0 +1,87 @@
+// <copyright file="EffectiveProviderEndpointTrackerTests.cs" company="Splunk Inc.">
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using Splunk.OpenTelemetry.AutoInstrumentation.EffectiveConfig;
+
+namespace Splunk.OpenTelemetry.AutoInstrumentation.Tests;
+
+public class EffectiveProviderEndpointTrackerTests
+{
+    [Fact]
+    public void Capture_ReplacesEndpointsAndReportsWhetherTheyChanged()
+    {
+        IReadOnlyList<EffectiveOtlpEndpoint> resolvedEndpoints =
+            [EffectiveOtlpEndpoint.Http("http://first-collector:4318/v1/traces")];
+        var tracker = CreateTracker(_ => resolvedEndpoints);
+
+        var provider = new object();
+        Assert.True(tracker.Capture(provider));
+        Assert.False(tracker.Capture(provider));
+
+        resolvedEndpoints = [EffectiveOtlpEndpoint.Http("http://second-collector:4318/v1/traces")];
+
+        Assert.True(tracker.Capture(provider));
+        Assert.Equal(resolvedEndpoints, tracker.GetEndpoints());
+    }
+
+    [Fact]
+    public void Capture_MarksTrackerUnhealthyUntilResolutionSucceeds()
+    {
+        var resolutionFails = false;
+        var tracker = CreateTracker(
+            _ => resolutionFails
+                ? throw new InvalidOperationException("resolution failed")
+                : [EffectiveOtlpEndpoint.Http("http://collector:4318/v1/traces")]);
+
+        var provider = new object();
+        Assert.True(tracker.Capture(provider));
+
+        resolutionFails = true;
+
+        Assert.False(tracker.Capture(provider));
+        Assert.Throws<InvalidOperationException>(() => tracker.GetEndpoints());
+        Assert.False(tracker.Capture(provider));
+
+        resolutionFails = false;
+
+        Assert.False(tracker.Capture(provider));
+        Assert.Single(tracker.GetEndpoints());
+    }
+
+    [Fact]
+    public void GetEndpoints_ReturnsImmutableSnapshot()
+    {
+        IReadOnlyList<EffectiveOtlpEndpoint> resolvedEndpoints =
+            [EffectiveOtlpEndpoint.Http("http://first-collector:4318/v1/traces")];
+        var tracker = CreateTracker(_ => resolvedEndpoints);
+        var provider = new object();
+        tracker.Capture(provider);
+        var snapshot = tracker.GetEndpoints();
+
+        resolvedEndpoints = [EffectiveOtlpEndpoint.Http("http://second-collector:4318/v1/traces")];
+        tracker.Capture(provider);
+
+        Assert.Equal(
+            [EffectiveOtlpEndpoint.Http("http://first-collector:4318/v1/traces")],
+            snapshot);
+    }
+
+    private static EffectiveProviderEndpointTracker<object> CreateTracker(
+        Func<object, IReadOnlyList<EffectiveOtlpEndpoint>> resolver)
+    {
+        return new EffectiveProviderEndpointTracker<object>(resolver);
+    }
+}
---
+++ test/Splunk.OpenTelemetry.AutoInstrumentation.Tests/EffectiveYamlConfigTests.cs
@@ -0,0 +1,148 @@
+// <copyright file="EffectiveYamlConfigTests.cs" company="Splunk Inc.">
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Reflection;
+using Splunk.OpenTelemetry.AutoInstrumentation.EffectiveConfig;
+using Splunk.OpenTelemetry.AutoInstrumentation.EffectiveConfig.Serialization;
+
+namespace Splunk.OpenTelemetry.AutoInstrumentation.Tests;
+
+public class EffectiveYamlConfigTests
+{
+    [Fact]
+    public void YamlModel_AnnotatesEveryPublicProperty()
+    {
+        var properties = new[] { typeof(EffectiveYamlConfig) }
+            .Concat(typeof(EffectiveYamlConfig).GetNestedTypes(BindingFlags.NonPublic | BindingFlags.Public))
+            .SelectMany(type => type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly));
+
+        Assert.All(
+            properties,
+            property => Assert.NotNull(property.GetCustomAttribute<EffectiveYamlPropertyAttribute>()));
+    }
+
+    [Fact]
+    public void YamlModel_PreservesRequiredNulls()
+    {
+        AssertPreservesNull(
+            typeof(EffectiveYamlConfig.EffectiveAlwaysOnProfilingWithMemoryConfig),
+            nameof(EffectiveYamlConfig.EffectiveAlwaysOnProfilingWithMemoryConfig.MemoryProfiler));
+
+        static void AssertPreservesNull(Type declaringType, string propertyName)
+        {
+            var property = declaringType.GetProperty(propertyName);
+            Assert.NotNull(property);
+
+            var attribute = property.GetCustomAttribute<EffectiveYamlPropertyAttribute>();
+            Assert.NotNull(attribute);
+            Assert.True(attribute.PreserveNull);
+        }
+    }
+
+    [Fact]
+    public void Create_CapturesConfigFileMetadata()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot());
+
+        Assert.Equal("stable.yaml", config.OtelConfigFile);
+    }
+
+    [Fact]
+    public void Create_SerializesHttpEndpointAsOtlpHttp()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot(
+            traceEndpoints: [EffectiveOtlpEndpoint.Http("http://collector:4318/v1/traces")]));
+
+        var processor = config.TracerProvider!.Processors.Single();
+        var exporter = processor.Batch!.Exporter;
+        Assert.Null(processor.Simple);
+        Assert.Equal("http://collector:4318/v1/traces", exporter.OtlpHttp!.Endpoint);
+        Assert.Null(exporter.OtlpGrpc);
+    }
+
+    [Fact]
+    public void Create_PreservesSimpleProcessorType()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot(
+            traceEndpoints:
+            [
+                EffectiveOtlpEndpoint.Http(
+                    "http://collector:4318/v1/traces",
+                    EffectiveOtlpPipelineType.Simple)
+            ]));
+
+        var processor = config.TracerProvider!.Processors.Single();
+        Assert.Null(processor.Batch);
+        Assert.Equal(
+            "http://collector:4318/v1/traces",
+            processor.Simple!.Exporter.OtlpHttp!.Endpoint);
+    }
+
+    [Fact]
+    public void Create_SerializesGrpcEndpointAsOtlpGrpc()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot(
+            metricEndpoints:
+            [
+                EffectiveOtlpEndpoint.Grpc(
+                    "http://collector:4317/opentelemetry.proto.collector.metrics.v1.MetricsService/Export",
+                    EffectiveOtlpPipelineType.Periodic)
+            ]));
+
+        var exporter = config.MeterProvider!.Readers.Single().Periodic.Exporter;
+        Assert.Null(exporter.OtlpHttp);
+        Assert.Equal(
+            "http://collector:4317/opentelemetry.proto.collector.metrics.v1.MetricsService/Export",
+            exporter.OtlpGrpc!.Endpoint);
+    }
+
+    [Fact]
+    public void Create_UsesNullMemoryProfilerMarkerWhenEnabled()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot(memoryProfilerEnabled: true));
+
+        var alwaysOn = Assert.IsType<EffectiveYamlConfig.EffectiveAlwaysOnProfilingWithMemoryConfig>(
+            config.Distribution!.Splunk!.Profiling!.AlwaysOn);
+        Assert.Null(alwaysOn.MemoryProfiler);
+    }
+
+    [Fact]
+    public void Create_OmitsMemoryProfilerMarkerWhenDisabled()
+    {
+        var config = EffectiveYamlConfig.Create(CreateSnapshot(cpuProfilerEnabled: true));
+
+        Assert.IsType<EffectiveYamlConfig.EffectiveAlwaysOnProfilingConfig>(
+            config.Distribution!.Splunk!.Profiling!.AlwaysOn);
+    }
+
+    private static EffectiveConfigSnapshot CreateSnapshot(
+        IReadOnlyList<EffectiveOtlpEndpoint>? traceEndpoints = null,
+        IReadOnlyList<EffectiveOtlpEndpoint>? metricEndpoints = null,
+        bool cpuProfilerEnabled = false,
+        bool memoryProfilerEnabled = false)
+    {
+        return new EffectiveConfigSnapshot(
+            fileBasedConfigFileName: "stable.yaml",
+            traceEndpoints: traceEndpoints ?? [],
+            metricEndpoints: metricEndpoints ?? [],
+            logEndpoints: [],
+            cpuProfilerEnabled: cpuProfilerEnabled,
+            memoryProfilerEnabled: memoryProfilerEnabled,
+            snapshotProfilerEnabled: false,
+            cpuProfilerCallStackInterval: 10000,
+            snapshotSamplingInterval: 40);
+    }
+}
